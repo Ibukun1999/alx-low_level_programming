66759445a8d7d7317d9d07094daa9e21a4816f69
Ibukun1999 <110995304+Ibukun1999@users.noreply.github.com>
2022-09-19 21:39:21 +0100

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,11 @@
 #include "main.h"
-/**
- * reset_to_98 - reset argument to 98
- * @n: pointer
- * Return: void
-*/
 
+/**
+ * reset_to_98 - resets value to 98
+ *
+ * @n: pointer integer
+ */
 void reset_to_98(int *n)
 {
-
-*n = 98;
-
+	*n = 98;
 }
